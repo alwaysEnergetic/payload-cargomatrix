2208e3c17601298609dfec488123e617e207f1ef
Soft1015 <softwaremodular@gmail.com>
2024-06-17 15:45:19 -0400

    update

--- src/app/_blocks/HeroBlock/SplitWithScreenshot.tsx
+++ src/app/_blocks/HeroBlock/SplitWithScreenshot.tsx
@@ -117,7 +117,7 @@ export const SplitWithScreenshot: React.FC<SplitWithScreenshotProps> = ({ layout
             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               {/* @ts-expect-error */}
-              { logo && <PayloadImage image={imageUrl} width={2432} height={1442} className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10" /> }
+              { imageUrl && <PayloadImage image={imageUrl} width={2432} height={1442} className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10" /> }
               </div>
             </div>
           </div>
